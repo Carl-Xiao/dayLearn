75a912dd9ae3f7fb9a8fa286007246caf21b87a7
Carl-Xiao <270555686@qq.com>
2020-11-08 18:41:23 +0800

    add

--- algorithm/Tree/BinaryTree/main.go
+++ algorithm/Tree/BinaryTree/main.go
@@ -192,13 +192,6 @@ func maxDepth(root *TreeNode) int {
 	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
 }
 
-func max(x, y int) int {
-	if x < y {
-		return y
-	}
-	return x
-}
-
 // 二叉树的最小深度
 // 给定一个二叉树，找出其最小深度。
 // 叶子节点到根节点
@@ -276,6 +269,29 @@ func buildTree(preorder []int, inorder []int) *TreeNode {
 	}
 
 	return c(preLength, inLength, len(inorder)-1, preorder, inorder)
+}
+
+// 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+
+	//进入下一层
+	left := lowestCommonAncestor(root.Left, p, q)
+
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left == nil {
+		return right
+	} else if right == nil {
+		return left
+	} else {
+		return root
+	}
 }
 
 func main() {
