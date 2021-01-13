29b82d7979fa56227f28e5cf36b25b075728b6c1
Carl <270555686@qq.com>
2021-01-13 21:43:17 +0800

    update

---
+++ algorithm/Jan/1.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+//普通解法
+func twoSum(nums []int, target int) []int {
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
+}
+
+//进阶解法
+func twoSumSecond(nums []int, target int) []int {
+	numsMap := make(map[int]int)
+
+	for index, value := range nums {
+		if i, ok := numsMap[target-value]; ok {
+			return []int{index, i}
+		} else {
+			numsMap[value] = index
+		}
+	}
+	return []int{}
+}
+
+func main() {
+	fmt.Println(twoSumSecond([]int{3, 2, 4}, 6))
+
+}
