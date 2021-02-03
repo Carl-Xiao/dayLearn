3048ed2d455ce83a13a92f8afa67d7d16a7660a3
carl-Xiao <270555686@qq.com>
2021-02-03 21:11:18 +0800

    update

--- algorithm/feb/main.go
+++ algorithm/feb/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 //ListNode 节点
 type ListNode struct {
 	Val  int
@@ -42,4 +47,68 @@ func swapPairs(head *ListNode) *ListNode {
 	head.Next = swapPairs(tmp.Next)
 	tmp.Next = head
 	return tmp
+}
+
+//滑动窗口
+// 480. 滑动窗口中位数
+// 中位数是有序序列最中间的那个数。如果序列的长度是偶数，则没有最中间的数；此时中位数是最中间的两个数的平均数。
+
+// 例如：
+
+// [2,3,4]，中位数是 3
+// [2,3]，中位数是 (2 + 3) / 2 = 2.5
+// 给你一个数组 nums，有一个长度为 k 的窗口从最左端滑动到最右端。窗口中有 k 个数，每次窗口向右移动 1 位。你的任务是找出每次窗口移动后得到的新窗口中元素的中位数，并输出由它们组成的数组。
+
+// 示例：
+
+// 给出 nums = [1,3,-1,-3,5,3,6,7]，以及 k = 3。
+
+// 窗口位置                      中位数
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       1
+//  1 [3  -1  -3] 5  3  6  7      -1
+//  1  3 [-1  -3  5] 3  6  7      -1
+//  1  3  -1 [-3  5  3] 6  7       3
+//  1  3  -1  -3 [5  3  6] 7       5
+//  1  3  -1  -3  5 [3  6  7]      6
+//  因此，返回该滑动窗口的中位数数组 [1,-1,-1,3,5,6]。
+
+func medianSlidingWindow(nums []int, k int) (ans []float64) {
+	//如果K为偶数,则不存在中位数
+	knums := make([]int, k)
+	copy(knums, nums[:k])
+	sort.Ints(knums)
+	ans = append(ans, midle(knums, k))
+
+	for i := k; i < len(nums); i++ {
+		id := sort.SearchInts(knums, nums[i-k]) //窗口左移动 左边的元素移除
+		knums[id] = nums[i]                     //窗口右边元素加入列表
+		//特殊情况如果替换的第一个元素
+		for id < k-1 && knums[id] > knums[id+1] {
+			knums[id], knums[id+1] = knums[id+1], knums[id]
+			id++
+		}
+		//替换的其他元素
+		for id > 0 && knums[id] < knums[id-1] {
+			knums[id], knums[id-1] = knums[id-1], knums[id]
+			id--
+		}
+		ans = append(ans, midle(knums, k))
+	}
+	return ans
+}
+
+//计算中位数
+func midle(nums []int, k int) float64 {
+	n1, n2 := 0, 0
+	if k%2 == 0 {
+		n1, n2 = nums[k/2-1], nums[k/2]
+	} else {
+		n1, n2 = nums[k/2], nums[k/2]
+	}
+	return float64(n1+n2) / float64(2)
+}
+
+func main() {
+	fmt.Println(medianSlidingWindow([]int{1, 3, -1, -3, 5, 3, 6, 7}, 3))
 }
